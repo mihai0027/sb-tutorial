253d3051cc293fc5eb2639583b16ec8eb77cc0d6
Mihai <mgold@MGold.lan>
2023-05-18 08:53:50 +0200

    Setting up the basics for project

---
+++ src/main/java/com/sb/sbtutorial/SbTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.sb.sbtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTutorialApplication.class, args);
+	}
+
+}
