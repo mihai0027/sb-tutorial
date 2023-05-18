feddc3ee3b215bab8712c42914b3dd9454fd57b3
Mihai <mgold@MGold.lan>
2023-05-18 14:51:13 +0200

    added a simple HelloController

---
+++ src/main/java/com/sb/sbtutorial/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.sb.sbtutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hi from a Demo App";
+    }
+    
+}
