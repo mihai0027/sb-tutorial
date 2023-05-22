674b75c129ada9cfa833974945627a7cd21a860f
Mihai <mgold@MGold.lan>
2023-05-22 14:56:07 +0200

    link to FAQ on custom queries

--- src/main/java/com/sb/sbtutorial/repository/DepartmentRepository.java
+++ src/main/java/com/sb/sbtutorial/repository/DepartmentRepository.java
@@ -1,11 +1,11 @@
 package com.sb.sbtutorial.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
 
 import com.sb.sbtutorial.entity.Department;
 
-// @Repository
+// How to create custom queries for JPA:
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     
     public Department findByDepartmentName(String departmentName);
